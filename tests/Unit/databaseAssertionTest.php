e7adddb9f75f2c6a7a9da5216025ca9ca3d57872
dami <goldendarmie@gmail.com>
2023-06-06 23:07:15 +0100

    made tests

--- tests/Unit/databaseAssertionTest.php
+++ tests/Unit/databaseAssertionTest.php
@@ -13,7 +13,7 @@ class databaseAssertionTest extends TestCase
      */
     public function test_example()
     {
-        $email = 'test_git@yahoo.comxs';
+        $email = 'test_git@gmail.com';
 
         // Assert that a user with the specified email exists in the database
         $this->assertDatabaseHas('users', [
